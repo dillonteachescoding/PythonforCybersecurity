dcd7724834914be0d61a77a6ec7b68f03171f0f0
dillonteachescoding <dillon.kierce@greenriver.edu>
2024-07-11 18:37:55 -0700

    Added our chapter 2 assignment.

---
+++ Lesson1.py
@@ -0,0 +1,9 @@
+#This takes a user input for name and saves it name
+name = input("What is your name: ")
+
+#Printing the name that was suggested above
+print(f"Hello, {name}!")
+
+#Print a affirmation
+print("Hi! You’re going to have a great day!")
+
