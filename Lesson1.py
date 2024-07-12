7dd7a1931ebf89e4c25e5cc4b81e7b0709460123
dillonteachescoding <dillon.kierce@greenriver.edu>
2024-07-11 18:44:44 -0700

    Added to end of script

--- Lesson1.py
+++ Lesson1.py
@@ -7,3 +7,11 @@ print(f"Hello, {name}!")
 #Print a affirmation
 print("Hi! You’re going to have a great day!")
 
+#We are prompting users for there age
+age = int(input("What is your age? "))
+
+#We want to take there age and add 2 to it
+new_age = age + 300
+
+#We will print the added age
+print(new_age)
