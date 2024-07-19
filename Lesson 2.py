ad2d5b8ef52008d326dfe0fa27203babc479afb2
dillonteachescoding <dillon.kierce@greenriver.edu>
2024-07-18 18:58:11 -0700

    perfece

--- Lesson 2.py
+++ Lesson 2.py
@@ -33,3 +33,23 @@ elif grade > 70:
 else:
     print("Failed")
 
+#Create a script that prompts user for a number, and then
+#tells the user if number is even
+
+#Prompt user for number
+number = int(input("Give a number: "))
+
+#Check if the value is even or odd
+if number % 2 == 0:
+    print(f"{number} is even!")
+else:
+    print(f"{number} is false!")
+
+
+#Prompt a user with a question
+answer = input("Is today a good day? (y/n): ")
+
+#Check that response if user did yes, or no
+if answer.lower() == 'y':
+    for _ in range (10):
+        print("Yea it is")
